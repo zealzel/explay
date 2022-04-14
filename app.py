1aaae72c009202df5d5f9ae564ea542ce0f9ae4f
zealzel <zealzel@gmail.com>
2022-04-14 14:13:52 +0800

    Add two merge_files examples

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-from explay.source import xlManager, ExPlay
+from explay.source import ExPlay
 from explay.utils import pd_set_option
 
 pd_set_option(max_colwidth=80, max_columns=15)
 
-HOME1 = "examples/test1/"
 PROJECT1 = "project1"
 PROJECT2 = "project2"
 
+home_typ_merge_files = "examples/typical/merge_files_2"
+proj_typ_merge_files = "project"
+home = home_typ_merge_files
+proj = proj_typ_merge_files
+
 
 if __name__ == "__main__":
-
-    home = HOME1
-    project = PROJECT2
-
-    ee = ExPlay(home, project)
+    ee = ExPlay(home=home, proj_name=proj)
     ee.run_proj(to_excel=False)
 
-    out = df.assign(orders=df.orders.str.strip().str.split("[\s、，/&]+")).explode(
-        "orders"
-    )
-    out.to_excel("out.xlsx")
+    #  out = df.assign(orders=df.orders.str.strip().str.split("[\s、，/&]+")).explode(
+    #  "orders"
+    #  )
+
+    df.to_excel("out.xlsx")
 
 """
 ref: https://stackoverflow.com/questions/47571618/how-to-split-expand-a-string-value-into-several-pandas-dataframe-rows
