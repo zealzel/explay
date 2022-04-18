3d20c11fe2a902ff662a14c9317784c678bd5273
zealzel <zealzel@gmail.com>
2022-04-18 09:31:01 +0800

    Simplify project.yml for all merge_xxx cases. No need for parser.

--- app.py
+++ app.py
@@ -3,27 +3,18 @@ from explay.utils import pd_set_option
 
 pd_set_option(max_colwidth=80, max_columns=15)
 
-home_2p = "examples/hr-bonus-2p"
-
-home_typ_merge_all = "examples/typical/merge_all"
-
-home_typ_merge_sheets = "examples/typical/merge_sheets"
-
-home_typ_merge_files_1 = "examples/typical/merge_files_1"
-home_typ_merge_files_2 = "examples/typical/merge_files_2"
+home_typ_merge_all = "test/example_projects/merger/merge_all"
+home_typ_merge_sheets = "test/example_projects/merger/merge_sheets"
+home_typ_merge_files = "test/example_projects/merger/merge_files"
 
 
 if __name__ == "__main__":
-    home = home_typ_merge_all
+    home = home_typ_merge_sheets
     proj = "project"
     ee = ExPlay(home=home, proj_name=proj)
     ee.run_proj(to_excel=False)
     ee.export_inputs()
     ee.export_parsers()
-
-    #  out = df.assign(orders=df.orders.str.strip().str.split("[\s、，/&]+")).explode(
-    #  "orders"
-    #  )
 
     #  df.to_excel("out.xlsx")
 
