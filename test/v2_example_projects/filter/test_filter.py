1011ff0e2a4a20385e82aeaca93da8eaeda0d609
zealzel <zealzel@gmail.com>
2022-04-19 10:56:25 +0800

    Fix tests for v2 yaml files

---
+++ test/v2_example_projects/filter/test_filter.py
@@ -0,0 +1,14 @@
+import pytest
+from explay.source import ExPlay
+
+
+@pytest.fixture()
+def exp1():
+    home = "test/v2_example_projects/filter/case1"
+    exp = ExPlay(home=home, proj_name="project")
+    return exp
+
+
+def test_filter_case1(exp1):
+    exp1.run_proj(to_excel=False)
+    assert True
