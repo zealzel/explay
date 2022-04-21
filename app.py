dd7bbce55676b41cc386b6449c2feff59c24e055
zealzel <zealzel@gmail.com>
2022-04-21 08:25:24 +0800

    1. Remove package Fire dependency in setup.py 2. Remove unused codes in app.py

--- app.py
+++ app.py
@@ -1,57 +1,32 @@
-import os
-import sys
-import shutil
-import json
-from pathlib import Path
-from explay.source import ExPlay
-from explay.utils import pd_set_option
-from pretty_html_table import build_table
-from explay.utils import register_func
+import argparse
 import warnings
+from pathlib import Path
 
+from explay.source import ExPlay
+from explay.utils import pd_set_option, register_func
 
-#  warnings.filterwarnings("ignore")
 warnings.simplefilter("ignore")
-
-
 pd_set_option(max_colwidth=80, max_columns=15)
-
-home_merge_all = "test/example_projects/merger/merge_all"
-home_merge_sheets = "test/example_projects/merger/merge_sheets"
-home_merge_files = "test/example_projects_v1/merger/merge_files"
-home_filter_1 = "test/v2_example_projects/filter/case1"
-home_filter_2 = "test/v2_example_projects/filter/case2"
-
-home_yaml = "test/v2_example_projects/yaml/"
-home_2p = "test/v2_example_private/hr-bonus-2p/"
-home_4p = "test/v2_example_private/hr-bonus-4p/"
-
-home = "/Users/zealzel/Downloads/My/code/執勤注意力各月清冊"
 
 
 if __name__ == "__main__":
-
-    import argparse
+    """
+    Usage:
+        python app.py --home=/path/to/proj/dir --proj=project
+    """
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--home", type=str, help="project home directory", default="")
     parser.add_argument("--proj", type=str, help="project name", default="project")
     args = parser.parse_args()
-
-    #  home = home_filter_2
-    #  proj = "project"
-
-    home = args.home
-    proj = args.proj
+    home, proj = args.home, args.proj
 
     workdir = str(Path(home).resolve())
     register_func(workdir)
     ee = ExPlay(home=workdir, proj_name=proj)
     ee.run_proj(to_excel=False)
-    ee.export_inputs()
-
-    ee.inputs["df"].to_excel("output.xlsx")
 
     #  ee.inputs["df"].to_excel("out.xlsx")
+    #  ee.export_inputs()
     #  ee.export_parsers()
     #  ee.export_html(projname="excel")
--- explay/source.py
+++ explay/source.py
@@ -283,10 +283,10 @@ class ExPlay:
     def merge_all(
         self, conv_name, xlsx_dir=None, sheet_name=0, excludes=None, save=False
     ):
-
         source_path = self._get_abs_source_path(xlsx_dir)
         #  self.merger = xlMerger(self._conv, source_path)
         #  df_merged, file_names = self.merger.merge_all(conv_name, sheet_name, excludes)
+
 
 
         merger = xlMerger(self._conv, source_path)
